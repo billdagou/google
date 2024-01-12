4d977551a20fb19be2d9a31e4ccfb10b0e157f1d
Bill.大狗 <billdagou@gmail.com>
2024-01-13 03:07:21 +0800

    Rename NS

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 defined('TYPO3') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['g'] = [
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['google'] = [
     'Dagou\\Google\\ViewHelpers',
 ];
