9cef9718400032d4c339d079ea13744b7e4b4a58
Bill.大狗 <billdagou@gmail.com>
2024-01-12 23:51:27 +0800

    Fix CSP

---
+++ Configuration/ContentSecurityPolicies.php
@@ -0,0 +1,11 @@
+<?php
+return \TYPO3\CMS\Core\Type\Map::fromEntries([
+    \TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope::frontend(),
+    new \TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationCollection(
+        new \TYPO3\CMS\Core\Security\ContentSecurityPolicy\Mutation(
+            \TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationMode::Extend,
+            \TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive::ScriptSrc,
+            new \TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue('www.googletagmanager.com')
+        )
+    )
+]);
